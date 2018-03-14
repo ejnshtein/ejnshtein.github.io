5961cfa59c9b35ff13231b4b5174c6e83291f0ab
ejnshtein <ejnshtein@gmail.com>
2018-03-14 19:42:29 +0200

    version 0.0.2b

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-## ejnshtein.github.io  
-[Overlay for Floric](https://ejnshtein.github.io)
-Yep, it is
+# [ejnshtein.github.io](ejnshtein.github.io)
+## What is this?  
+It's an overlay for stream via [OBS](https://obsproject.com/).
+Working from browser source
+## Installation
+Not needed. Just open ur browser   https://ejnshtein.github.io/?channelId=channelid&apiKey=apikey&channelIdtw=twitchchannel&apiKeytw=apikeytwitch aaaaaaannnnnnnnnnnnnnnnd soon i'll done this
--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // Subs count from YT and Twitch
 
-
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
@@ -10,13 +9,10 @@ function getParameterByName(name, url) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
-console.log(getParameterByName('token'),getParameterByName('groupid'))
 let channelId = getParameterByName('channelId'), // For Youtube
     apiKey = getParameterByName('apiKey'), // Also for YouTube
     channelIdtw = getParameterByName('channelIdtw'), // For Twitch
-    apiKeytw = getParameterByName('apiKeytw'), // Also for Twitch
-    token = getParameterByName('token'), // for VK
-    groupId = getParameterByName('groupid') // Also for VK
+    apiKeytw = getParameterByName('apiKeytw') // Also for Twitch
 function setData(type, content) {
     document.getElementById(type).innerHTML = content;
 }
@@ -25,13 +21,13 @@ function subs() {
     xmlhttp.onreadystatechange = function () {
         var myObj = {};
         if (this.responseText) {
-            myObj = JSON.parse(this.responseText);
+            myObj = JSON.parse(this.responseText)
         }
-        if (Object.keys(myObj).indexOf("items") !== -1) {
+        if (myObj.items !== undefined) {
             setData('yt', myObj.items[0].statistics.subscriberCount)
         }
     }
-    var json = 'https://www.googleapis.com/youtube/v3/channels?part=statistics&id=' + channelId + '&key=' + apiKey
+    var json = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${apiKey}`
     xmlhttp.open("GET", json, true);
     xmlhttp.send();
 }
@@ -41,35 +37,20 @@ function subsT() {
     xmlhttp.onreadystatechange = function () {
         var myObj = {};
         if (this.responseText) {
-            myObj = JSON.parse(this.responseText);
+            try {
+                myObj = JSON.parse(this.responseText)
+            } catch (e){
+                return
+            }
         }
-        if (Object.keys(myObj).indexOf("_total") !== -1) {
+        if (myObj._total !== undefined) {
             setData('twitch', myObj._total)
         }
     }
-    var json = 'https://api.twitch.tv/kraken/channels/' + channelIdtw + '/follows?client_id=' + apiKeytw
+    var json = `https://api.twitch.tv/kraken/channels/${channelIdtw}/follows?client_id=${apiKeytw}`
     xmlhttp.open("GET", json, true);
     xmlhttp.send();
 }
-function subsVk(){
-    let xmlhttp = new XMLHttpRequest(),
-        json = `https://api.vk.com/method/groups.getMembers?group_id=${groupId}&access_token=${token}&v=5.71&count=10`
-    xmlhttp.onreadystatechange = () => {
-        let myObj
-        if (this.responseText){
-            myObj = JSON.parse(this.responseText)
-        }
-        console.log(myObj)
-        if (Object.keys(myObj).indexOf('response') !== -1){
-            setData('vk', myObj.response.count)
-        }
-        console.log(this.responseText)
-    }
-    xmlhttp.open("GET", json, true);
-    xmlhttp.setRequestHeader('Access-Control-Allow-Origin','*')
-    xmlhttp.send();
-}
-subsVk()
 subs()
 subsT()
 setInterval(() => {
