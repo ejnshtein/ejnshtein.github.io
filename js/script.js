a5e90bf77c81473377c3f8961536ee7706abf139
ejnshtein <ejnshtein@gmail.com>
2018-03-14 19:16:52 +0200

    v. 0.0.2

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
 // Subs count from YT and Twitch
 
-// Сдесь редактирование файла ('' - обязательны)
 
-var channelId = 'UCZfvLX7vf4rSd8OJFujgvSg' // For Youtube
-var apiKey = 'AIzaSyBk8xION30TEINPmWFPsYJXFxyMd51ZvBY' // Also for YouTube
-var channelIdtw = 'oleglivanov' // For Twitch
-var apiKeytw = 's28lwjbid3b8ihhw22r7s1q2o0bjxz' // Also for Twitch
-
-// Конец редактирования
-function startSubs(content) {
-    document.getElementById("Subs").innerHTML = content;
-};
-
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+console.log(getParameterByName('token'),getParameterByName('groupid'))
+let channelId = getParameterByName('channelId'), // For Youtube
+    apiKey = getParameterByName('apiKey'), // Also for YouTube
+    channelIdtw = getParameterByName('channelIdtw'), // For Twitch
+    apiKeytw = getParameterByName('apiKeytw'), // Also for Twitch
+    token = getParameterByName('token'), // for VK
+    groupId = getParameterByName('groupid') // Also for VK
+function setData(type, content) {
+    document.getElementById(type).innerHTML = content;
+}
 function subs() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
@@ -20,16 +28,12 @@ function subs() {
             myObj = JSON.parse(this.responseText);
         }
         if (Object.keys(myObj).indexOf("items") !== -1) {
-            startSubs(myObj.items[0].statistics.subscriberCount)
+            setData('yt', myObj.items[0].statistics.subscriberCount)
         }
     }
-    var json = ('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=' + channelId + '&key=' + apiKey);
+    var json = 'https://www.googleapis.com/youtube/v3/channels?part=statistics&id=' + channelId + '&key=' + apiKey
     xmlhttp.open("GET", json, true);
     xmlhttp.send();
-}
-
-function startSubsT(content) {
-    document.getElementById("TSubs").innerHTML = content;
 }
 
 function subsT() {
@@ -40,16 +44,35 @@ function subsT() {
             myObj = JSON.parse(this.responseText);
         }
         if (Object.keys(myObj).indexOf("_total") !== -1) {
-            startSubsT(myObj._total)
+            setData('twitch', myObj._total)
         }
     }
-    var json = ('https://api.twitch.tv/kraken/channels/' + channelIdtw + '/follows?client_id=' + apiKeytw);
+    var json = 'https://api.twitch.tv/kraken/channels/' + channelIdtw + '/follows?client_id=' + apiKeytw
     xmlhttp.open("GET", json, true);
     xmlhttp.send();
 }
+function subsVk(){
+    let xmlhttp = new XMLHttpRequest(),
+        json = `https://api.vk.com/method/groups.getMembers?group_id=${groupId}&access_token=${token}&v=5.71&count=10`
+    xmlhttp.onreadystatechange = () => {
+        let myObj
+        if (this.responseText){
+            myObj = JSON.parse(this.responseText)
+        }
+        console.log(myObj)
+        if (Object.keys(myObj).indexOf('response') !== -1){
+            setData('vk', myObj.response.count)
+        }
+        console.log(this.responseText)
+    }
+    xmlhttp.open("GET", json, true);
+    xmlhttp.setRequestHeader('Access-Control-Allow-Origin','*')
+    xmlhttp.send();
+}
+subsVk()
 subs()
 subsT()
-setInterval(()=>{
+setInterval(() => {
     subs()
     subsT()
-},10000)
+}, 10000)
