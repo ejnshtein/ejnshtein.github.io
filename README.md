ce066cd91a4198f7d35b514cfc76f904a5fe976c
ejnshtein <ejnshtein@gmail.com>
2018-03-14 23:23:41 +0200

    ᕦ( ͡° ͜ʖ ͡°)ᕤ

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 It's an overlay for stream via [OBS](https://obsproject.com/).
 Working from browser source
 ## Installation
-Not needed. Just open ur browser   https://ejnshtein.github.io/?channelId=channelid&apiKey=apikey&channelIdtw=twitchchannel&apiKeytw=apikeytwitch aaaaaaannnnnnnnnnnnnnnnd soon i'll done this
+Not needed. Just open ur browsersource in obs https://ejnshtein.github.io/?channelId=channelid&apiKey=apikey&channelIdtw=twitchchannel&apiKeytw=apikeytwitch aaaaaaannnnnnnnnnnnnnnnd soon i'll done this readme
--- js/script.js
+++ js/script.js
@@ -1,59 +1,80 @@
 // Subs count from YT and Twitch
 
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
+function getParameterByName(name, url) { // From stackoverflow https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+    if (!url) url = window.location.href
+    name = name.replace(/[\[\]]/g, "\\$&")
     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+        results = regex.exec(url)
+    if (!results) return null
+    if (!results[2]) return ''
+    return decodeURIComponent(results[2].replace(/\+/g, " "))
 }
-let channelId = getParameterByName('channelId'), // For Youtube
-    apiKey = getParameterByName('apiKey'), // Also for YouTube
-    channelIdtw = getParameterByName('channelIdtw'), // For Twitch
-    apiKeytw = getParameterByName('apiKeytw') // Also for Twitch
+let channelId = getParameterByName('channelId'), // Youtube channel id
+    apiKey = getParameterByName('apiKey'), // YouTube api key
+    channelIdtw = getParameterByName('channelIdtw'), // Twitch channel id
+    apiKeytw = getParameterByName('apiKeytw') // Twitch api key
+switch (true){ // Seems beautify then a lot of if(){}
+    case !channelId:
+        console.error('Youtube channel id is undefined!')
+        break
+    case !apiKey:
+        console.error('Youtube apikey is undefined!')
+        break
+    case !channelIdtw:
+        console.error('Twitch channel id is undefined')
+        break
+    case !apiKeytw:
+        console.error('Twitch apikey is undefined!')
+        break
+}
+
 function setData(type, content) {
     document.getElementById(type).innerHTML = content;
 }
-function subs() {
-    var xmlhttp = new XMLHttpRequest();
+function subs() { // Full by me ( ͡° ͜ʖ ͡°)
+    let xmlhttp = new XMLHttpRequest(),
+        json = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${apiKey}`
+    if (!channelId || !apiKey) return
     xmlhttp.onreadystatechange = function () {
-        var myObj = {};
-        if (this.responseText) {
-            myObj = JSON.parse(this.responseText)
-        }
-        if (myObj.items !== undefined) {
-            setData('yt', myObj.items[0].statistics.subscriberCount)
-        }
-    }
-    var json = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${apiKey}`
-    xmlhttp.open("GET", json, true);
-    xmlhttp.send();
-}
-
-function subsT() {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        var myObj = {};
+        let myObj
         if (this.responseText) {
             try {
                 myObj = JSON.parse(this.responseText)
             } catch (e){
-                return
+                return console.error('Wrong response from YouTube!\nSometimes it\'s happend, don\'t panic ( ͡° ͜ʖ ͡°)')
             }
         }
-        if (myObj._total !== undefined) {
+        if (myObj != undefined && myObj.items != undefined) {
+            setData('yt', myObj.items[0].statistics.subscriberCount)
+        }
+    }
+    xmlhttp.open("GET", json, true);
+    xmlhttp.send();
+}
+
+function subsTwitch() { // ᕦ( ͡° ͜ʖ ͡°)ᕤ
+    let xmlhttp = new XMLHttpRequest(),
+        json = `https://api.twitch.tv/kraken/channels/${channelIdtw}/follows?client_id=${apiKeytw}`
+    if (!channelIdtw || !apiKeytw) return   
+    xmlhttp.onreadystatechange = function () {
+        let myObj
+        if (this.responseText) {
+            try {
+                myObj = JSON.parse(this.responseText)
+            } catch (e){
+                return console.error('Wrong response from Twitch!\nSometimes it\'s happend, don\'t panic ( ͡° ͜ʖ ͡°)')
+            }
+        }
+        if (myObj != undefined && myObj._total != undefined) {
             setData('twitch', myObj._total)
         }
     }
-    var json = `https://api.twitch.tv/kraken/channels/${channelIdtw}/follows?client_id=${apiKeytw}`
     xmlhttp.open("GET", json, true);
     xmlhttp.send();
 }
 subs()
-subsT()
+subsTwitch()
 setInterval(() => {
     subs()
-    subsT()
+    subsTwitch()
 }, 10000)
